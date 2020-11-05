f4032752dd41ea9b57359ecfb9724b56367d4fcd
AirGear45996 <airgear45996@hotmail.com>
2020-11-05 09:16:23 +0700

    Убрал костыльный api

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,22 +20,7 @@ export default {
   plugins: [ // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
     '@/plugins/globals',
     '@/plugins/element-ui',
-      '@/plugins/axios',
-
-    //region поддключение this.$api(controller,method,params = {})
-
-    // Ппредставим, что у нас на странице находится 20 компонентов которые запрашивают данные через API,
-    // таким образом за один запрос страницы c Nuxt сервером будет установлено 20 дополнительных внутренних http
-    // соединений.
-    // Nuxt HTTP сервер может обрабатывать более 55 тысяч запросов в секунду, однако создавая внутренние HTTP запросы,
-    // мы уменьшаем потенциальные ресурсы сервера в десятки раз.
-    // Но ведь когда мы рендерим страницу на сервере, у нас есть прямой доступ до всех контроллеров в папке /api/
-
-    // Эти плагины решают данную проблему:
-    '@/plugins/api-context.client.js', // отрабатывает на клиенте, создаёт ajax запрос через axios
-    '@/plugins/api-context.server.js'  // отрабатывает на сервере, напрямую обращается к controller через required
-    //endregion
-
+    '@/plugins/axios',
   ],
   components: true, // Auto import components (https://go.nuxtjs.dev/config-components)
   buildModules: [ // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
@@ -45,24 +30,7 @@ export default {
     '@nuxtjs/axios', // https://go.nuxtjs.dev/axios
     '@nuxtjs/markdownit', // https://www.npmjs.com/package/@nuxtjs/markdownit
   ],
-  serverMiddleware: [
-      //region api
-      // Подключим наш API Server к Nuxt через serverMiddleware
-      //... обработка параметров request.params -> конвертируем в json
-      { path: "/api", handler: require("body-parser").json() },
-      {
-        path: "/api",
-        handler: (req, res, next) => {
-          const url = require("url");
-          req.query = url.parse(req.url, true).query;
-          req.params = { ...req.query, ...req.body };
-          next();
-        }
-      },
-      //.... обработчик внешних запросов ( от клиента )
-      { path: "/api", handler: "@/serverMiddleware/api-server.js" }
-      //endregion
-  ],
+  serverMiddleware: [],
   markdownit: {
     // [optional] markdownit options
     // See https://github.com/markdown-it/markdown-it
